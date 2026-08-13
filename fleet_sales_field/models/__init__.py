7066aa32d8986deb8609acaac582dbc4552b4f4b
One-Ayman <125468307+One-Ayman@users.noreply.github.com>
2026-08-13 17:51:01 +0300

    Delete fleet_sales_field directory

--- fleet_sales_field/models/__init__.py
+++
@@ -1,7 +0,0 @@
-from . import res_company
-from . import res_config_settings
-from . import res_partner
-from . import res_users
-from . import account_payment
-from . import sale_order
-from . import fleet_sales_visit
